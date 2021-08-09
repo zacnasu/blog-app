fdc0c164143805af8e11a23d89682588166477ef
Zachary Nasu <zacnasu@gmail.com>
2021-08-08 17:19:09 -0700

    added ejs files with bootstrap

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res) {
-    res.send("success")
+    res.render("home")
 });
+
+// for later once database is created
+// app.get("/:articleName", function(req, res){
+//     req.params.articleName
+// })
 
 app.listen(process.env.PORT || 5000);
 module.exports = app;
