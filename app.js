534503d33958586896412d838434757eab02cacb
Zachary Nasu <zacnasu@gmail.com>
2021-08-08 21:48:17 -0700

    added test code for fetching database data

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
+const AWS = require('aws-sdk')
 const express = require("express");
 const app = express();
 
+AWS.config.region = process.env.REGION;
+
+const title_table = process.env.TITLE_DATABASE_NAME;
+const posts_table = process.env.POSTS_DATABASE_NAME;
+
+const database = new AWS.DynamoDB();
+
 app.set('view engine', 'ejs');
 
+
 app.get("/", function(req, res) {
-    res.render("home")
+    res.render("home");
 });
 
-// for later once database is created
-// app.get("/:articleName", function(req, res){
-//     req.params.articleName
-// })
+app.get("/:articleName", function(req, res){
+    params = {
+        TableName: title_table,
+        KeyConditionExpression: 'title_id = i:',
+        ExpressionAttributeValues: {
+            ':i': req.params.articleName
+        }
+    }
+    database.query(params, function(err, data){
+        if(err){
+            res.send("failed")
+        }else{
+            res.send(data)
+        }
+    })
+});
 
 app.listen(process.env.PORT || 5000);
 module.exports = app;
